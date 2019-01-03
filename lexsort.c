b21951b61164fb92913aa7aca353630685eb090b
Elliot Friedman <elliot@theden.io>
2019-01-02 21:36:34 -0800

    Insertion sort algorithm finished

--- lexsort.c
+++ lexsort.c
@@ -6,7 +6,7 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/15 23:19:21 by efriedma          #+#    #+#             */
-/*   Updated: 2018/12/17 12:18:23 by efriedma         ###   ########.fr       */
+/*   Updated: 2019/01/02 20:57:53 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,33 @@ void	lex_sort(char **str, int argc)
 	int	i;
 	int	x;
 	
-	i = 0;
+	i = i;
 	x = argc;
 	while (i < argc)
 	{
 		int	y = 0;
 		int z = i;
-		while (i + 1 < argc && str[i][y]
-				&& str[i + 1][y] && str[i + 1][y] == str[i][y])
+		//iterate past all equal characters in adjacent strings
+		while (i + 1 < argc && str[i][y] && str[i + 1][y] && str[i + 1][y] == str[i][y])
 			y++;
+		//go through string z times and while the current string is greater than the next string
 		while (z > -1 && z + 1 < argc && str[z + 1][y] < str[z][y])
 		{
-			while (z + 1 < argc && str[z][y]
-					&& str[z + 1][y] && str[z + 1][y] == str[z][y])
+			//iterate past all equal characters
+			while (z + 1 < argc && str[z][y] && str[z + 1][y] && str[z + 1][y] == str[z][y])
 				y++;
+
+			//swap the next string with the current string
 			tmp = str[z];
 			str[z] = str[z + 1];
 			str[z + 1] = tmp;
+			//go from right to left
 			z--;
+			y = 0;
 		}
 		i++;
 		int j = 1;
-		printf("\n");
+		printf("i: %d\n", i - 1);
 		while (j < argc)
 		{
 			printf("%s\n", str[j]);
@@ -64,10 +69,10 @@ int main(int argc, char **argv)
 {
 	if (argc > 1)
 		lex_sort(argv, argc);
-	int i = 0;
+	int i = 1;
 	while (i < argc)
 	{
-		printf("%s\n", argv[i]);
+		printf("\n%s\n", argv[i]);
 		i++;
 	}
 	return 0;
