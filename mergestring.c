07332e7cfbf84f76fc8cdade6f331cebff9c7d04
Elliot Friedman <elliot@theden.io>
2019-01-05 18:43:07 -0800

    Merge sort done

--- mergestring.c
+++ mergestring.c
@@ -6,7 +6,7 @@
 /*   By: efriedma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 01:11:56 by efriedma          #+#    #+#             */
-/*   Updated: 2019/01/05 16:14:30 by efriedma         ###   ########.fr       */
+/*   Updated: 2019/01/05 18:41:41 by efriedma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,111 +19,94 @@ typedef struct	s_str
 	struct s_str	*next;
 }					t_str;
 
-int			listlen(t_str *ret)
+void		printlist(t_str *a)
 {
-	int		i;
-
-	i = 0;
-	while (ret)
+	while (a)
 	{
-		ret = ret->next;
-		i++;
+		printf("a: %s\n", a->str);
+		a = a->next;
 	}
-	return (i);
 }
 
-//merge everything into ret
-t_str		*merge(t_str *a, t_str *b)
+t_str* SortedMerge(t_str *a, t_str* b)
 {
-	int		cmp;
-	//implement these two
-	t_str	*head;
-	t_str	*ret;
+	t_str* result;
 
-	//find the first string and set your head to it
-	head = ft_strcmp(a->str, b->str) < 0 ? a : b;
-	a = ft_strcmp(a->str, b->str) < 0 ? a->next : a;
-	b = ft_strcmp(a->str, b->str) > 0 ? b->next : b;
-	ret = head;
-	while (42)
+	result = 0;
+	if (!a)
+		return (b);
+	else if (!b)
+		return (a);
+	if (ft_strcmp(a->str, b->str) < 0)
 	{
-		printf("Merging: a %s b %s\n", a->str, b->str);
-		if (a == 0 && b == 0)
-			return (ret);
-		//check if the strings of a and b match up, cmp will be 0 if so
-		if (!(cmp = ft_strcmp(a->str, b->str)))
-		{
-			head->next = cmp < 0 ? a : b;
-			a->next = cmp < 0 ? a->next : a;
-			b->next = cmp > 0 ? b->next : b;
-		}
-		//if the strings are equal, then what do we do?
-		if (!cmp)
-		{
-			//set next value to a
-			head->next = a ? a : b;
-			//set a to the next value otherwise it is already 0
-			a = a ? a->next : 0;
-			//if there wasn't a value at a, iterate b
-			b = !a ? b->next : b;
-		}
-		printf("Merging: head %s\n", head->str);	
-		head = head->next;
+		result = a;
+		result->next = SortedMerge(a->next, b);
 	}
-	return (ret);
+	else
+	{
+		result = b;
+		result->next = SortedMerge(a, b->next);
+	}
+	return (result);
+}
+
+void		frontBackSplit(t_str *head, t_str **a, t_str **b)
+{
+	t_str	*slow;
+	t_str	*fast;
+
+	fast = head->next;
+	slow = head;
+	while (fast)
+	{
+		fast = fast->next;
+		if (fast)
+		{
+			fast = fast->next;
+			slow = slow->next;
+		}
+	}
+	*b = slow->next;
+	*a = head;
+	slow->next = 0;;
 }
 
 t_str		*mergeSort(t_str **ret)
 {
-	//split linked list into two halves
 	t_str	*a;
 	t_str	*b;
-	int		len;
+	t_str	*head;
 
-	len = listlen(*ret);
 	a = *ret;
-	//	b = split(ret);
-	printf("Ret value: %s\n", (*ret)->str);
-	if (!*ret || len == 1)
+	head = *ret;
+	if (!head || !head->next)
 		return (0);
-	len /= 2;
-	while (len)
-	{
-		a = a->next;
-		len--;
-	}
-	b = a->next;
-	a->next = 0;
-	mergeSort(ret);
+	frontBackSplit(head, &a, &b);
+	mergeSort(&a);
 	mergeSort(&b);
-	*ret = merge(*ret, b);
+	*ret = SortedMerge(a, b);
 	return (*ret);
 }
 
-void		addvalues(t_str *ret, char **argv, int argc)
+t_str		*buildlist(char **argv, int argc)
 {
-	int i;
+	t_str	*head;
+	t_str	*ret;
+	int		i;
 
 	i = 1;
+	ret = ft_memalloc(sizeof(t_str));
+	head = ret;
 	while (i < argc)
 	{
 		ret->len = (int)ft_strlen(argv[i]);
 		ret->str = argv[i];
-		printf("%s\n", argv[i]);
+		if (i + 1 < argc)
+			ret->next = ft_memalloc(sizeof(t_str));
+		ret = ret->next;
 		i++;
 	}
-}
-
-void		buildlist(t_str **ret, int argc)
-{
-	argc -= 2;
-	(*ret) = ft_memalloc(sizeof(t_str));
-	while (argc > 0)
-	{
-		(*ret)->next = ft_memalloc(sizeof(t_str));
-		(*ret) = (*ret)->next;
-		argc--;
-	}
+	return (head);
 }
 
 t_str		*sortedstrings(char **argv, int argc)
@@ -132,8 +115,8 @@ t_str		*sortedstrings(char **argv, int argc)
 
 	if (argc < 2)
 		exit(0);
-	buildlist(&ret, argc);
-	addvalues(ret, argv, argc);
+	ret = buildlist(argv, argc);
+	printlist(ret);
 	return (mergeSort(&ret));
 }
 
@@ -141,6 +124,7 @@ int main(int argc, char **argv)
 {
 	t_str *sorted = sortedstrings(argv, argc);
 
+	printf("\n\n\n\n\n\n");
 	while (sorted)
 	{
 		printf("%s\n", sorted->str);
